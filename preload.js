12b3050294687e4d955cac7b2c75cde45dee5b24
william-forsyth <w.forsyth94@gmail.com>
2026-05-31 16:12:40 +1000

    feat: expose folder sidebar API in preload

--- preload.js
+++ preload.js
@@ -9,4 +9,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
   toggleFullscreen: () => ipcRenderer.send('window:fullscreen'),
   onMaximizeChange: (cb) => ipcRenderer.on('window:maximized', (_, val) => cb(val)),
   onFullscreenChange: (cb) => ipcRenderer.on('window:fullscreened', (_, val) => cb(val)),
+
+  // Folder sidebar
+  pickFolderRoot:    ()      => ipcRenderer.invoke('folder:pick-root'),
+  listDir:           (p)     => ipcRenderer.invoke('folder:list-dir', p),
+  loadSidebarState:  ()      => ipcRenderer.invoke('folder:load-state'),
+  saveSidebarState:  (state) => ipcRenderer.invoke('folder:save-state', state),
 });
